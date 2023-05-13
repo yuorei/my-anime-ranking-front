a912aa7221d4be32a2af84545f3ef20666fa4d66
yuorei <yuorei71@gmail.com>
2023-05-13 22:00:14 +0900

    login

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,60 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { ApolloClient, InMemoryCache, ApolloProvider, HttpLink } from '@apollo/client';
-import App from './App.jsx'
-import './index.css'
-
-const client = new ApolloClient({
-  link: new HttpLink({
-    uri: '',
-  }),
-  cache: new InMemoryCache(),
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import App from "./App.jsx";
+import "./index.css";
+// const url=import.meta.env.VITE_URL
+const httpLink = createHttpLink({
+  uri: "http://localhost:8080/query",
 });
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem("token");
+  // return the headers to the context so httpLink can read them
+  if (!token) {
+    return { headers };
+  }
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+// const authLink = setContext((_, { headers }) => {
+//   // get the authentication token from local storage if it exists
+//   const token = localStorage.getItem('token');
+//   // return the headers to the context so httpLink can read them
+//   return {
+//     headers: {
+//       ...headers,
+//       authorization: token ? `Bearer ${token}` : "",
+//     }
+//   }
+// });
+
+// const httpLink = new HttpLink({
+//   uri: url,
+// });
+
+// const client = new ApolloClient({
+//   link: authLink.concat(httpLink),
+//   cache: new InMemoryCache(),
+// });
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ApolloProvider client={client}>
       <App />

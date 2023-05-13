661f52091a76daee2a7c1a92984cb6e5b5ae333c
yuorei <yuorei71@gmail.com>
2023-05-13 11:10:26 +0900

    add ApolloClient

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ApolloClient, InMemoryCache, ApolloProvider, HttpLink } from '@apollo/client';
 import App from './App.jsx'
 import './index.css'
 
+const client = new ApolloClient({
+  link: new HttpLink({
+    uri: '',
+  }),
+  cache: new InMemoryCache(),
+});
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </React.StrictMode>
+);

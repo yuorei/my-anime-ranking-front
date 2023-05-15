bb083e8e0eb5b9c729c7668cb8188f9a2b933857
yuorei <yuorei71@gmail.com>
2023-05-15 18:07:37 +0900

    react-hooks →　client

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useMutation } from "@apollo/react-hooks";
+import { useMutation } from "@apollo/client";
 import gql from "graphql-tag";
 
 const LOGIN_MUTATION = gql`
